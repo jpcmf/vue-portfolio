18a931abf0ec1412a9c12128c7c52da4ea4a5957
João Paulo <musicjoaopaulo@gmail.com>
2018-09-24 20:31:05 -0300

    Add twitter link to about

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -10,7 +10,7 @@
           </header>
           <!-- <div class="image about"></div> -->
           <span class="image main"><img src="../../assets/about.jpg" alt=""></span>
-          <p>I am a UI Designer and Front-end Developer from Brazil based in Curitiba. I like working as freelancer, solving problems with Design and making great ideas happen. I love sharing the knowledge that I have gained from my experience with other people. When I am not developing websites I like to produce electronic music and play some records. You can find me on Twitter (@jpcmf80) and, if you're interested in my professional experience, please have a look at my links or drop me a message.</p>
+          <p>I am a UI Designer and Front-end Developer from Brazil based in Curitiba. I like working as freelancer, solving problems with Design and making great ideas happen. I love sharing the knowledge that I have gained from my experience with other people. When I am not developing websites I like to produce electronic music and play some records. You can find me on Twitter (<a href="https://www.twitter.com/jpcmf80" target="_blank">@jpcmf80</a>) and, if you're interested in my professional experience, please have a look at my links or drop me a message.</p>
         </div>
       </section>
 
